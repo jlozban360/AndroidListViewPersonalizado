12471c1256c81851601504b7da0fa7634d1bd7cd
jlozban360 <jlozban360@g.educaand.es>
2024-10-14 14:00:02 +0200

    Initial release

--- app/src/main/java/es/iesoretania/listviewpersonalizado/ActivityModulos.java
+++ app/src/main/java/es/iesoretania/listviewpersonalizado/ActivityModulos.java
@@ -1,8 +1,6 @@
 package es.iesoretania.listviewpersonalizado;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,46 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import es.iesoretania.listviewpersonalizado.databinding.ActivityMainBinding;
 import es.iesoretania.listviewpersonalizado.databinding.ActivityModulosBinding;
 
 public class ActivityModulos extends AppCompatActivity {
+    private ActivityModulosBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_modulos); // Asegúrate de que este layout tenga los IDs correctos
-
+        setContentView(R.layout.activity_modulos);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Inicializa los elementos de la vista
-        ImageView imageView = findViewById(R.id.imageView); // Asegúrate de que este ID coincida con tu layout
-        TextView textViewNombre = findViewById(R.id.textViewNombre);
-        TextView textViewApellidos = findViewById(R.id.textViewApellidos);
-        TextView textViewCiclo = findViewById(R.id.textViewModulo);
-        // Obtener datos del Intent
-        String nombre = getIntent().getStringExtra("nombre");
-        String apellidos = getIntent().getStringExtra("apellidos");
-        String ciclo = getIntent().getStringExtra("ciclo");
-        String sexo = getIntent().getStringExtra("sexo"); // Asegúrate de enviar el sexo desde MainActivity
+        binding = ActivityModulosBinding.inflate( getLayoutInflater( ) );
+        setContentView( binding.getRoot( ) );
 
-        // Mostrar la información en los TextView
-        textViewNombre.setText(nombre);
-        textViewApellidos.setText(apellidos);
-        textViewCiclo.setText(ciclo);
 
-        // Cargar el avatar según el sexo
-        if (sexo != null) {
-            if (sexo.equals("Masculino")) {
-                imageView.setImageResource(R.drawable.avatar_hombre); // Reemplaza con tu imagen
-            } else {
-                imageView.setImageResource(R.drawable.avatar_mujer); // Reemplaza con tu imagen
-            }
-        }
     }
 }
