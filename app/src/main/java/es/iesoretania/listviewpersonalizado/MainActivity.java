a91270690343fc07b1146fa62c4255fca7eabd7d
jlozban360 <jlozban360@g.educaand.es>
2024-10-14 13:56:27 +0200

    Initial release

--- app/src/main/java/es/iesoretania/listviewpersonalizado/MainActivity.java
+++ app/src/main/java/es/iesoretania/listviewpersonalizado/MainActivity.java
@@ -1,7 +1,14 @@
 package es.iesoretania.listviewpersonalizado;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +19,74 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-{
-    private ListView listViewSimple;
-    private List<String> personasList = new ArrayList<>();
+import es.iesoretania.listviewpersonalizado.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ListView listViewSimple;
+    private List<Personas> personasList = new ArrayList<>(); // Cambiado a List<Personas>
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        listViewSimple = findViewById( R.id.lis)
+        listViewSimple = binding.listViewPersonas;
+
+        personasList.add(new Personas("Juan", "Pérez", "Masculino", "DAM"));
+        personasList.add(new Personas("Ana", "García", "Femenino", "DAW"));
+        personasList.add(new Personas("Jose", "Lozano", "Masculino", "ASIR"));
+
+        ArrayAdapter<Personas> adapter = new ArrayAdapter<Personas>(this, 0, personasList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.item_persona, parent, false);
+                }
+
+                ImageView imageViewAvatar = convertView.findViewById(R.id.imageView);
+                TextView textViewNombre = convertView.findViewById(R.id.textViewNombre);
+                TextView textViewApellidos = convertView.findViewById(R.id.textViewApellidos);
+                TextView textViewCiclo = convertView.findViewById(R.id.textViewModulo); // Asegúrate de que este ID coincida
+
+                Personas persona = getItem(position);
+
+                if (persona != null) {
+                    textViewNombre.setText(persona.getNombre());
+                    textViewApellidos.setText(persona.getApellidos());
+                    textViewCiclo.setText(persona.getCiclo());
+
+                    if(persona.getSexo().equals("Masculino"))
+                        imageViewAvatar.setImageResource(R.drawable.avatar_hombre);
+                    else
+                        imageViewAvatar.setImageResource(R.drawable.avatar_mujer);
+                }
+
+                return convertView;
+            }
+        };
+
+        listViewSimple.setAdapter(adapter);
+
+        listViewSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Personas persona = personasList.get(position);
+                Intent intent = new Intent(MainActivity.this, ActivityModulos.class);
+                intent.putExtra("nombre", persona.getNombre());
+                intent.putExtra("apellidos", persona.getApellidos());
+                intent.putExtra("ciclo", persona.getCiclo());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/es/iesoretania/listviewpersonalizado/Personas.java
+++ app/src/main/java/es/iesoretania/listviewpersonalizado/Personas.java
@@ -17,31 +17,18 @@ public class Personas {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
     public String getApellidos() {
         return apellidos;
     }
 
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
 
     public String getSexo() {
         return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
     }
 
     public String getCiclo() {
         return ciclo;
     }
 
-    public void setCiclo(String ciclo) {
-        this.ciclo = ciclo;
-    }
 }
